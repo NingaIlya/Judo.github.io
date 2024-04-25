74ae0f2e5476343fd3280ffd87f47cb02098097a
NingaIlya <168070893+NingaIlya@users.noreply.github.com>
2024-04-25 10:40:37 +0300

    Add files via upload

---
+++ Coding/script.js
@@ -0,0 +1,59 @@
+//let user = {
+ //Name: 'Ilya',
+ //age: 11,
+ //Email: 'kolosovs.ilya@gmail.com',
+ 
+//kolosov: function(){
+  //console.log(`Привет, меня зовут ${user.Name} и мне ${user.age} лет, и мой имэйл ${uaer.Email}.`);
+//}
+//}
+  //console.log(user)
+  //user.get.info = 'ilya kolosov'
+
+  //for (let key in user) {
+  //console.log(user[key])
+   //
+   
+   
+   
+  // let car ={
+  //brand: 'Audi',
+ // madel: 1425-2722,
+  //year: 1854,
+
+//whatcar: function() {
+ // console.log(`У меня бренд ${car.brand} а ещё у меня модель ${car.madel} жалко что старая она ${car.year}.`);
+//}
+//}
+//console.log(car)
+
+//car.start = 'Машина начала ехать!'
+
+ //for (let w in car) {
+ //  console.log(car[w])
+ //}
+ 
+
+
+ //let array1 = [1, 2, 3];
+//let array2 = [4, 5, 6];
+
+
+//array1.push(...array2);
+
+//console.log(array1); 
+
+//let array = [1, 2, 3];
+
+//let dilet = array.pop();
+
+//console.log("Удалёная цыфра из массива:", dilet); 
+//console.log("Изменёный массив", array);
+
+//let ilya1 = [1, 2, 3, 4, 5];
+//let ilya2 = [6, 7, 8, 9, 10];
+
+//ilya1.push(...ilya2);
+
+//console.log(ilya1);
+
